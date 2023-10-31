988ddb1f9311ad63e964da9e3f8f6face383bdf8
Samuel <samuel@iMac-de-Andres.local>
2023-10-31 14:34:50 -0500

    initial files

---
+++ README.md
@@ -0,0 +1,4 @@
+# AirBnb Clone - Console
+
+The project is a console whose main utility is to manipulate data for an AirBnb clone database
+
---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This module will define the BaseModel class, with 
+common attributes/methods for other classes
+"""
+
+
+import datetime
+import uuid
+
+
+class BaseModel:
+    
