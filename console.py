0316fb361559a396579087af410af7886d319409
Samuel <samuel@iMac-de-Andres.local>
2023-11-05 23:00:32 -0500

    task 9 test 2

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ This module contains the command interpreter for the AirBnB console
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
